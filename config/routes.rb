feac2c9996766dffb08be8f591e20169e56ed2bd
m-musha <gnmknr@gmail.com>
2021-05-04 09:07:51 +0000

    admin/memberのコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  
+  devise_for :members, controllers: {
+    sessions:      'members/sessions',
+    passwords:     'members/passwords',
+    registrations: 'members/registrations'
+  }
+  
+  #devise_for :users, controllers: {
+    #sessions: 'users/sessions'}
 end
